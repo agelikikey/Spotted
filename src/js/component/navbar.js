cdce835d6fa7ed7bca3adb9f67785cd436417a25
agelikikey <agelikikey@gmail.com>
2020-04-08 22:57:50 +0000

    took off home tab

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -19,11 +19,6 @@ export function Navbar() {
 			</button>
 			<div className="collapse navbar-collapse" id="navbarResponsive">
 				<ul className="navbar-nav ml-auto">
-					<li className="nav-item active">
-						<a className="nav-link" href="#">
-							Home <span className="sr-only">(current)</span>
-						</a>
-					</li>
 					<li className="nav-item">
 						<a className="nav-link" href="#">
 							Login
