c5786a99b6274acdfdcb4294e1776700ee764adc
Stephanie Moure <stephanie.moure@gmail.com>
2020-04-08 23:47:28 +0000

    added jumbotron

---
+++ src/js/component/jumbotron.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Jumbotron = props => (
+	<div className="jumbotron">
+		<h1 className="display-4">Spotted</h1>
+		<div className="col-auto">
+			<label className="sr-only" htmlFor="inlineFormInputGroup">
+				Search
+			</label>
+			<div className="input-group mb-2">
+				<div className="input-group-prepend">
+					<div className="input-group-text">
+						<i className="fas fa-search" />
+					</div>
+				</div>
+				<input type="text" className="form-control" id="inlineFormInputGroup" placeholder="Username" />
+			</div>
+		</div>
+	</div>
+);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Jumbotron } from "../component/jumbotron";
 
 export const Home = () => (
 	<div className="text-center mt-5">
+		<Jumbotron />
 		<h1>Hello Ladies! :) </h1>
 		<p />
 		<a href="#" className="btn btn-success">
