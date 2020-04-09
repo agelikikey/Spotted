5ac61f1a423c437aa64bb0699dbeda5005000a94
Stephanie Moure <stephanie.moure@gmail.com>
2020-04-09 00:39:07 +0000

    added cards

---
+++ src/js/component/card.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Card = props => (
+	<div className="card mb-3 mx-2 border border-secondary bg-light text-dark rounded" style={{ width: "18rem" }}>
+		<img id="card-img" src={props.img} className="card-img-top" alt="..." />
+		<div className="card-body">
+			<h5 className="card-title">{props.title}</h5>
+			<p className="card-text">{props.description}</p>
+		</div>
+		<ul className="list-group list-group-flush">
+			<li className="list-group-item">
+				<i className="fas fa-map-marker-alt">{props.location}</i>
+			</li>
+			<li className="list-group-item">{props.date}</li>
+		</ul>
+		<div className="card-body">
+			<a href="#" className="card-link">
+				View on map!
+			</a>
+		</div>
+	</div>
+);
+
+Card.propTypes = {
+	title: PropTypes.string,
+	img: PropTypes.string,
+	description: PropTypes.string,
+	location: PropTypes.string,
+	date: PropTypes.string
+};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,10 +2,34 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import { Jumbotron } from "../component/jumbotron";
+import { Card } from "../component/card";
 
 export const Home = () => (
 	<div className="text-center mt-5">
 		<Jumbotron />
+		<div className="card-group justify-content-around">
+			<Card
+				title="Toilet Paper"
+				img="https://mobileimages.lowes.com/product/converted/054000/054000133428.jpg?size=xl"
+				location="Publix on Biscayne Blvd"
+				description="Found TP today @ Publix for BOGO."
+				date="4 April 2020"
+			/>
+			<Card
+				title="Hand Sanitizer"
+				img="https://images-na.ssl-images-amazon.com/images/I/81gPULDQMZL._AC_SL1500_.jpg"
+				location="CVS on 7th"
+				description="Found Hand Sani, only allowing 1 per family. Have to get it at the register"
+				date="1 April 2020"
+			/>
+			<Card
+				title="Bread"
+				img="https://www.ambitiouskitchen.com/wp-content/uploads/2019/04/Multi-Grain-Seedy-Sandwich-Bread-Edited-5sq.jpg"
+				location="Aldi in Kendall"
+				description="All kinds of breaded just restocked"
+				date="7 April 2020"
+			/>
+		</div>
 		<h1>Hello Ladies! :) </h1>
 		<p />
 		<a href="#" className="btn btn-success">
