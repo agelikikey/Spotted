939bde3b1bd9a57c7890b0104325a5aaf583cac8
agelikikey <agelikikey@gmail.com>
2020-04-09 00:26:08 +0000

    div added

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,12 +4,22 @@ import "../../styles/home.scss";
 import { Jumbotron } from "../component/jumbotron";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<Jumbotron />
-		<h1>Hello Ladies! :) </h1>
-		<p />
-		<a href="#" className="btn btn-success">
-			Blah Blah Blah Blah
-		</a>
+	<div>
+		<div className="text-center mt-5">
+			<Jumbotron />
+			<h1>Hello Ladies! :) </h1>
+			<p />
+			<a href="#" className="btn btn-success">
+				Blah Blah Blah Blah
+			</a>
+		</div>
+		<div className="text-center mt-5">
+			<h4> JOIN THE MOVEMENT </h4>
+			<h6> FIND YOUR ESSENTIALS TODAY!</h6>
+			<p />
+			<a href="#" className="btn btn-success">
+				SIGN ME UP
+			</a>
+		</div>
 	</div>
 );
