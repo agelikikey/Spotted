5c1e06b3ae214882ec6f9ad869aecb2748b76f5e
Stephanie Moure <stephanie.moure@gmail.com>
2020-04-20 22:37:14 +0000

    added linkedin links to team pics, added link to sign up button on home page

--- src/js/component/team.js
+++ src/js/component/team.js
@@ -1,9 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export const Team = props => (
 	<div className="card mb-3 mx-2 border border-secondary bg-light text-dark rounded" style={{ width: "18rem" }}>
-		<img id="card-img" src={props.pic} className="card-img-top" alt="..." />
+		<a target="_blank" rel="noopener noreferrer" href={props.linkedin}>
+			<img id="card-img" src={props.pic} className="card-img-top" alt="..." />
+		</a>
 		<div className="card-body">
 			<h5 className="card-title">{props.member}</h5>
 			<p className="card-text">{props.bio}</p>
@@ -20,5 +23,6 @@ Team.propTypes = {
 	member: PropTypes.string,
 	pic: PropTypes.string,
 	bio: PropTypes.string,
-	homebase: PropTypes.string
+	homebase: PropTypes.string,
+	linkedin: PropTypes.string
 };
