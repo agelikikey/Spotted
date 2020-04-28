a9467cfb76a5f3a3279eec35abfc69376fb957f8
Brenda Matos <brenda.matosux@gmail.com>
2020-04-28 01:26:29 +0000

    modal updated with fields

--- src/js/component/buttonModal.js
+++ src/js/component/buttonModal.js
@@ -4,7 +4,7 @@ import { Modal } from "react-bootstrap";
 import "../../styles/home.scss";
 import "../../styles/index.scss";
 
-import rigoImage from "../../img/magnifyingGlass.svg";
+import magnifyingGlass from "../../img/magnifyingGlass.svg";
 
 export function ButtonModal() {
 	const [show, setShow] = useState(false);
@@ -18,10 +18,35 @@ export function ButtonModal() {
 			<Modal show={show} onHide={handleClose}>
 				<Modal.Header closeButton>
 					<Modal.Title>
-						<img src="magnifyingGlass.svg" alt="Magnifying Glass with Heart" height="80" width="80" />
+						<img src={magnifyingGlass.svg} alt="Magnifying Glass with Heart" height="80" width="80" />
 					</Modal.Title>
 				</Modal.Header>
-				<Modal.Body>Woohoo youre reading this text in a modal!</Modal.Body>
+				<Modal.Body>
+					<input
+						type="file"
+						className="form-control"
+						accept="image/*"
+						placeholder="Upload image of essential item"
+						aria-label="Image"
+						aria-describedby="basic-addon1"
+					/>
+
+					<label hmtlFor="items">Choose an essential:</label>
+					<select id="items">
+						<option value="toilet paper">Toilet Paper</option>
+						<option value="antibacterial soap">Antibacterial Soap</option>
+						<option value="disinfectant wipes">Disinfectant Wipes</option>
+						<option value="masks">Masks</option>
+					</select>
+
+					<label hmtlFor="basic-url">Enter your location</label>
+					<span className="input-group-text" id="basic-addon3">
+						<img src="my_location-24px 2.svg" alt="Compass" height="24" width="24" />
+					</span>
+					<input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" />
+
+					<textarea className="form-control" aria-label="With textarea" />
+				</Modal.Body>
 				<Modal.Footer>
 					<Button variant="secondary" onClick={handleClose}>
 						Close
