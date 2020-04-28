e4e8d43af85280e495f6d0662692ae0033b4bee1
Brenda Matos <brenda.matosux@gmail.com>
2020-04-28 23:41:35 +0000

    updated footer and team cards

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => (
-	<div className="mt-auto py-0 text-center bg-dark text-white bottom">
-		<footer className="page-footer font-small blue py-4">
+	<div className="mt-auto py-0 text-center text-white bottom coaFooter">
+		<footer className="page-footer font-small blue py-8">
+			<div className="text-center mt-5 py-3">
+				<h4> JOIN THE MOVEMENT </h4>
+				<h6> FIND YOUR ESSENTIALS TODAY!</h6>
+				<p />
+				<Link to={"/signup"} href="#" className="btn btn-light btnFooter">
+					SIGN ME UP
+				</Link>
+			</div>
 			<div className="footer-copyright text-center py-1">
 				Made with <i className="fa fa-heart text-danger" /> by
-				<a href="..."> Women Who Code</a>
+				<p> Women Who Code</p>
 			</div>
 		</footer>
 	</div>
