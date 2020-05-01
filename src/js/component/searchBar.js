653bb69c5c551bcfd2af78a90e5e7e8240393536
Stephanie Moure <stephanie.moure@gmail.com>
2020-05-01 01:04:21 +0000

    changed search

--- src/js/component/searchBar.js
+++ src/js/component/searchBar.js
@@ -1,44 +1,64 @@
-import React from "react";
+import React, { useState, useEffect, useContext, Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-export const SearchBar = () => (
-	<div className="col-auto input-group mb-3 siteSearch">
-		<label className="sr-only" htmlFor="inlineFormInputGroup">
-			Search
-		</label>
-		{/* <div className="input-group mb-2"> */}
-		<div className="input-group-prepend">
-			<div className="input-group-text bg-white">
-				<select className="essential-dropdown">
-					<option value="default">Select an essential:</option>
-					<option value="toilet paper">ToiletPaper</option>
-					<option value="antibacterial soap">Soap</option>
-					<option value="disinfectant wipes">Wipe</option>
-					<option value="masks">Mask</option>
-					<option value="other essentials">Other Essentials</option>
-				</select>
-			</div>
-		</div>
-		{/* <span className="input-group-text bg-white text-dark"> */}
-		<input
-			type="text"
-			className="form-control location-search"
-			id="inlineFormInputGroup"
-			placeholder="Enter your zip code"
-		/>
-		{/* </span> */}
-		<div className="input-group-append">
-			<button className="btn btn-outline-dark bg-white text-dark" type="button">
-				<Link to={"/results-wipes"}>
-					<i className="fas fa-search" />
-				</Link>
-			</button>
-		</div>
-		{/* </div> */}
-	</div>
-);
+export function SearchBar() {
+	const [essential, setEssential] = useState("");
 
-SearchBar.propTypes = {
-	match: PropTypes.object
-};
+	return (
+		<div className="col-auto input-group mb-3 siteSearch">
+			<label className="sr-only" htmlFor="inlineFormInputGroup">
+				Search
+			</label>
+			{/* <div className="input-group mb-2"> */}
+			<div className="dropdown">
+				<button
+					className="btn btn-secondary dropdown-toggle"
+					type="button"
+					id="dropdownMenuButton"
+					data-toggle="dropdown"
+					aria-haspopup="true"
+					aria-expanded="false">
+					Choose an Essential:{" "}
+				</button>
+				<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+					<a className="dropdown-item" href="#">
+						Toilet Paper
+					</a>
+					<a className="dropdown-item" href="#">
+						Masks
+					</a>
+					<a className="dropdown-item" href="#">
+						Wipes
+					</a>
+					<a className="dropdown-item" href="#">
+						Soap
+					</a>
+					<a className="dropdown-item" href="#">
+						Other Essentials
+					</a>
+				</div>
+			</div>
+			{/* <span className="input-group-text bg-white text-dark"> */}
+			<input
+				type="text"
+				className="form-control location-search"
+				id="inlineFormInputGroup"
+				placeholder="Enter your zip code"
+			/>
+			{/* </span> */}
+			<div className="input-group-append">
+				<button className="btn btn-outline-dark bg-white text-dark" type="button">
+					<Link to={"/results-wipes"}>
+						<i className="fas fa-search" />
+					</Link>
+				</button>
+			</div>
+			{/* </div> */}
+		</div>
+	);
+}
+
+// SearchBar.propTypes = {
+// 	match: PropTypes.object
+// };
